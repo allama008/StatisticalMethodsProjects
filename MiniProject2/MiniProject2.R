2a899705a1e852ecbcfa7cc038527cefea755db5
Allama Hossain <a2hossain2000@yahoo.com>
2022-03-04 10:31:50 -0600

    Bug fixes, code documentation and code improvement

--- MiniProject2/MiniProject2.R
+++ MiniProject2/MiniProject2.R
@@ -1,96 +1,58 @@
-
-#1
-##a
-roadrace_data =read.csv("C:/Users/allam/Documents/Spring 2022/CS6313 - Statistical Methods/MiniProjects/MiniProject2/roadrace.csv")
-head(roadrace_data$Age)
-View(roadrace_data)
-#barplot(c(sum(roadrace_data$Maine == "Away"), sum(roadrace_data$Maine == "Maine")), names.arg =c("Away", "Maine"), space = 0.25, ylab = "Number of runners")
-#sum(roadrace_data$Maine == "Away")
-#sum(roadrace_data$Maine == "Maine")
-
-Maine = sum(roadrace_data$Maine == "Maine")
-Away = sum(roadrace_data$Maine == "Away")
-barplot(c(Maine,Away),main ="Question1", ylab = "Number of runners", names=c("Maine","Away"),xlab="Type", col="#7CFFBB")
-
-Maine
-Away
-
-summary(roadrace_data$Maine)
-
-
-## a final
-
+# Question 1a)
 
 roadrace_data = read.csv("C:/Users/allam/Documents/Spring 2022/CS6313 - Statistical Methods/MiniProjects/MiniProject2/roadrace.csv")
 Maine = sum(roadrace_data$Maine == "Maine")
 Away = sum(roadrace_data$Maine == "Away")
+
 barplot(c(Maine,Away),main ="Question1", ylab = "Number of runners", names=c("Maine","Away"),
         xlab="Type", col="#7CFFBB")
 Maine
 Away
 summary(roadrace_data$Maine)
-##b
 
-Away = roadrace_data$Time..minutes.[which(roadrace_data$Maine == 'Away')] 
-hist(AwayRunnerTime, main = "Histogram - Runner Time (Away)", xlim = range(0,200), ylim = range(0,2000), 
+# Question 1b)
+
+away_runner_time = roadrace_data$Time..minutes.[which(roadrace_data$Maine == 'Away')] 
+hist(away_runner_time, main = "Histogram - Runner Time (Away)", xlim = range(0,200), ylim = range(0,600), 
      xlab = "Away Runner Time (minutes)", ylab = "Frequency", border = "black", col = "yellow" )
 
-Maine = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Maine")]
-hist(MaineRunnerTime, main = "Histogram - Runner Time (Maine)", xlim = range(0,200), ylim = range(0,2000),
+maine_runner_time = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Maine")]
+hist(maine_runner_time, main = "Histogram - Runner Time (Maine)", xlim = range(0,200), ylim = range(0,2000),
      xlab = "Maine Runner Time (minutes)", ylab = "Frequency" , border = "black", col = "yellow")
 
 
-range(Away)
-IQR(Away)
-sd(Away)
-mean(Away)
-median(Away)
+range(away_runner_time)
+IQR(away_runner_time)
+sd(away_runner_time)
+mean(away_runner_time)
+median(away_runner_time)
 
 
-range(Maine)
-IQR(Maine)
-sd(Maine)
-mean(Maine)
-median(Maine)
+range(maine_runner_time)
+IQR(maine_runner_time)
+sd(maine_runner_time)
+mean(maine_runner_time)
+median(maine_runner_time)
 
 
-summary(Away)
-summary(Maine)
+summary(away_runner_time)
+summary(maine_runner_time)
 
 
-##c
-Away = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Away")] 
-Maine = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Maine")]
-boxplot(Maine, Away, names = c("Maine Time", "Away Time"), main = "Box plot",ylab = "Frequency", 
+# Question 1c)
+away_runner_time = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Away")] 
+maine_runner_time = roadrace_data$Time..minutes.[which(roadrace_data$Maine == "Maine")]
+boxplot(maine_runner_time, away_runner_time, names = c("Maine", "Away"), main = "Runners' time (in minutes) for Maine and Away runners",ylab = "Frequency", 
         col = "yellow", border= "orange")
 
-##d
-
-
-
-#boxplot(MaleAge,FemaleAge,main="Boxplot - Male & Female Runners
-#Age",ylab="Male & Female
-#        RunnersAge",xlab="Age",col="dodgerblue4",border="black",names=c('MaleAge','FemaleAge'),notch= TRUE,horizontal = TRUE)
-
-
+# Question 1d)
 MaleAge = roadrace_data$Age[which(roadrace_data$Sex=='M')]
 FemaleAge = roadrace_data$Age[which(roadrace_data$Sex=='F')]
 
-min(MaleAge)
-
 MaleAge = as.numeric(as.character(MaleAge))
 FemaleAge = as.numeric(as.character(FemaleAge))
-min(as.numeric(MaleAge))
-mean(FemaleAge)
-#MaleAge<-as.numeric(MaleAge)
-#FemaleAge<-as.numeric(FemaleAge)
 
-boxplot(MaleAge,FemaleAge,main="Boxplot for Male & Female Runners Age",ylab="Male & Female
-          RunnersAge",xlab="Age",col="dodgerblue4",border="black",names=c('RunnersAge_Male','RunnersAge_
-                                                                          Female'),notch= TRUE,horizontal = TRUE)
-#boxplot(MaleAge, FemaleAge, names = c('Male Runners Age','Female Runners Age'))
-
-#boxplot(MaleAge,FemaleAge, main = "Box plot - Male & Female runners age", names = c("Maine", "Away"), ylab= "Runners age",col="green")
+boxplot(MaleAge, FemaleAge, main = "Box plot - Male & Female runners age", names = c("Male", "Female"), ylab= "Runners age",col="green")
 
 mean(MaleAge)
 median(MaleAge)
@@ -106,36 +68,8 @@ range(FemaleAge)
 IQR(FemaleAge)
 sd(FemaleAge)
 
-
-#####error here in ##d
-
-#2
-
-data1= read.csv("C:/Users/allam/Documents/Spring 2022/CS6313 - Statistical Methods/MiniProjects/MiniProject2/motorcycle.csv")
-FatalAccidents = data1$Fatal.Motorcycle.Accidents
-#Now create boxplot for Fatal.Motorcycle.Accidents values
-boxplot(FatalAccidents, xlab = "Fatal Motorcycle Accidents", ylab = "Number of Accidents") 
-#For calculating the required statistics, we are executing the following code
-#Now calculate the Upper and Lower bounds
-LowerBound = max(quantile(FatalAccidents, prob=0.25)- 1.5*IQR(FatalAccidents),min(FatalAccidents))
-UpperBound=min(quantile(FatalAccidents, prob=0.75) + 1.5*IQR(FatalAccidents),max(FatalAccidents))
-#Now check for outliers and display counties that are outliers
-FatalCounty=data1$County[which(data1$Fatal.Motorcycle.Accidents<LowerBound | data1$Fatal.Motorcycle.Accidents > UpperBound)]
-
-
-FatalCounty
-
-summary(FatalAccidents)
-range(FatalAccidents)
-IQR(FatalAccidents)
-sd(FatalAccidents)
-
-
-
-#2 final
-
-
-data1= read.csv("C:/Users/allam/Documents/Spring 2022/CS6313 - Statistical Methods/MiniProjects/MiniProject2/motorcycle.csv")
+# Question 2)
+data1 = read.csv("C:/Users/allam/Documents/Spring 2022/CS6313 - Statistical Methods/MiniProjects/MiniProject2/motorcycle.csv")
 FatalAccidents = data1$Fatal.Motorcycle.Accidents
 
 boxplot(FatalAccidents, main = "BoxPlot - Motorcycle Accidents", xlab = "Fatal Accidents", ylab = "Accidents count", col="orange") 
@@ -144,13 +78,16 @@ boxplot(FatalAccidents, main = "BoxPlot - Motorcycle Accidents", xlab = "Fatal A
 LowerBound = max(quantile(FatalAccidents, prob=0.25)- 1.5*IQR(FatalAccidents),min(FatalAccidents))
 UpperBound=min(quantile(FatalAccidents, prob=0.75) + 1.5*IQR(FatalAccidents),max(FatalAccidents))
 
+#Outlier details
 FatalCounty=data1$County[which(data1$Fatal.Motorcycle.Accidents<LowerBound | data1$Fatal.Motorcycle.Accidents > UpperBound)]
-
-
 FatalCounty
 
 summary(FatalAccidents)
 range(FatalAccidents)
 IQR(FatalAccidents)
 sd(FatalAccidents)
+
+# Start addition by Allama on outlier details.
+outlierValues <- boxplot.stats(FatalAccidents)$out
+subset(data1, Fatal.Motorcycle.Accidents == outlierValues, c(County, Fatal.Motorcycle.Accidents))
 
